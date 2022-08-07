12571e4227ecd1a8022e3182fb800e3fe4b0d66d
PedroNnsM <pedronunes95@outlook.com>
2022-08-06 21:48:40 -0300

    novo paciente

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,6 @@
+
+
+
 var pacientes = document.querySelectorAll(".paciente");
 
 for (var i = 0; i < pacientes.length; i++) {
@@ -34,4 +37,58 @@ for (var i = 0; i < pacientes.length; i++) {
         tdImc.textContent = imc.toFixed(2);
         
     }
+} 
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+
+botaoAdicionar.addEventListener("click", function(event) {
+    event.preventDefault();
+    
+     var form = document.querySelector("#form-adiciona");
+    
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var nome = form.nome.value;
+    var gordura = form.gordura.value;
+    
+    var pacienteTr = document.createElement("tr");
+    pacienteTr.classList.add("paciente");
+
+    var nomeTd = document.createElement("td");
+    nomeTd.classList.add("info-nome");
+    nomeTd.textContent = nome;
+    pacienteTr.appendChild(nomeTd);
+
+    var pesoTd = document.createElement("td");
+    pesoTd.classList.add("info-peso");
+    pesoTd.textContent = peso;
+    pacienteTr.appendChild(pesoTd);
+
+    var alturaTd = document.createElement("td");
+    alturaTd.classList.add("info-altura");
+    alturaTd.textContent = altura;
+    pacienteTr.appendChild(alturaTd);
+
+    var gorduraTd = document.createElement("td");
+    gorduraTd.classList.add("info-gordura");
+    gorduraTd.textContent = gordura;
+    pacienteTr.appendChild(gorduraTd);
+
+    
+    // var paciente = obtemPacienteDoFormulario(form);
+    
+    // var erros = validaPaciente(paciente);
+    
+    // if (erros.length > 0) {
+    //     exibeMensagensDeErro(erros);
+    //     return;
+    // }
+    
+    // adicionaPacienteNaTabela(paciente);
+    
+    // form.reset();
+    
+    // var mensagensErro = document.querySelector("#mensagens-erro");
+    // mensagensErro.innerHTML = "";
 }
+);
