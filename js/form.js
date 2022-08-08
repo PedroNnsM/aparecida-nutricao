acf4c131c736cc2574d7a3119dd4e92e5d6f62aa
PedroNnsM <pedronunes95@outlook.com>
2022-08-07 21:44:55 -0300

    refatorando

--- js/form.js
+++ js/form.js
@@ -1,15 +1,45 @@
-var titulo = document.querySelector(".titulo");
-titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+botaoAdicionar.addEventListener("click", function(event) {
+    event.preventDefault();
+    
+     var form = document.querySelector("#form-adiciona");
+    
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var nome = form.nome.value;
+    var gordura = form.gordura.value;
+    
+    var pacienteTr = document.createElement("tr");
+    pacienteTr.classList.add("paciente");
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+    var nomeTd = document.createElement("td");
+    nomeTd.classList.add("info-nome");
+    nomeTd.textContent = nome;
+    pacienteTr.appendChild(nomeTd);
 
-var tdImc = paciente.querySelector(".info-imc");
+    var pesoTd = document.createElement("td");
+    pesoTd.classList.add("info-peso");
+    pesoTd.textContent = peso;
+    pacienteTr.appendChild(pesoTd);
 
-var imc = peso / (altura * altura);
-tdImc.textContent = imc;
+    var alturaTd = document.createElement("td");
+    alturaTd.classList.add("info-altura");
+    alturaTd.textContent = altura;
+    pacienteTr.appendChild(alturaTd);
+
+    var gorduraTd = document.createElement("td");
+    gorduraTd.classList.add("info-gordura");
+    gorduraTd.textContent = gordura;
+    pacienteTr.appendChild(gorduraTd);
+
+
+    var imcTd = document.createElement("td");
+    imcTd.classList.add("info-imc");
+    imcTd.textContent = calculaImc(peso, altura);
+    pacienteTr.appendChild(imcTd);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+} );
