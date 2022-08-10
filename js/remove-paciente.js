3cf2723ac93312e82754801e9ee7ed7a3d5853c8
PedroNnsM <pedronunes95@outlook.com>
2022-08-10 15:57:22 -0300

    setTimeout

--- js/remove-paciente.js
+++ js/remove-paciente.js
@@ -1,7 +1,13 @@
 var pacientes = document.querySelectorAll(".paciente");
 
-pacientes.forEach(function(paciente) {
-    paciente.addEventListener("dblclick", function() {
-        this.remove();
-    } )
-} )
+var tabela = document.querySelector("table");
+
+
+tabela.addEventListener("dblclick", function(event) {
+        event.target.parentNode.classList.add("fadeOut");
+        setTimeout(function() {
+                event.target.parentNode.remove();
+        }, 500);
+}
+);
+ 
