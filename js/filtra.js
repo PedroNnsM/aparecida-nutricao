523e2a74ccd0776ec5ff54a6fb423b9ad1664544
Pedro Nunes Machado <100473453+PedroNnsM@users.noreply.github.com>
2022-08-11 12:06:23 -0300

    filtro por letra

--- js/filtra.js
+++ js/filtra.js
@@ -1,11 +1,28 @@
 var campoFiltro = document.querySelector("#filtrar-tabela");
 
 campoFiltro.addEventListener("input", function() {
+    console.log(this.value);
     var pacientes = document.querySelectorAll(".paciente");
 
-    for (var i = 0; i < pacientes.length; i++) {
-        var paciente = pacientes[i];
-        var tdNome = paciente.querySelector(".info-nome");
-        var nome = tdNome.textContent;
+    if(this.value.length > 0){
+        console.log('tem algo digitado')
+        
+        for (var i = 0; i < pacientes.length; i++) {
+            var paciente = pacientes[i];
+            var tdNome = paciente.querySelector(".info-nome");
+            var nome = tdNome.textContent;
+            var expressao = new RegExp(this.value, "i")
+
+            if( !expressao.test(nome)){
+                paciente.classList.add("invisivel")
+            }else{
+                paciente.classList.remove("invisivel")
+            }
+        }
+    }else{
+        for(var i = 0; i < pacientes.length; i++){
+            var paciente = paciente[i]
+            paciente.classList.remove("invisivel")
+        }
     }
 });
