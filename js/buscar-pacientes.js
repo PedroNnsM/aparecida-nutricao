842aa3951bf6d72d268d9bf47fbacc99ddcdd556
Pedro Nunes Machado <100473453+PedroNnsM@users.noreply.github.com>
2022-08-13 23:05:57 -0300

    :smile:

---
+++ js/buscar-pacientes.js
@@ -0,0 +1,27 @@
+var botaoAdicionar = document.querySelector("#buscar-pacientes");
+
+botaoAdicionar.addEventListener("click", function(){
+    console.log("Buscando pacientes...");
+
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
+
+    xhr.addEventListener("load", function() {
+        var erroAjax = document.querySelector("#erro-ajax");
+        if (xhr.status == 200) {
+            erroAjax.classList.add("fadeOut");
+            var resposta = xhr.responseText;
+            var pacientes = JSON.parse(resposta);
+
+            pacientes.forEach(function(paciente) {
+                adicionaPacienteNaTabela(paciente);
+            });
+        } else {
+            erroAjax.classList.remove("fadeOut");
+        }
+    });
+
+    xhr.send();
+});
+
